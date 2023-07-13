3fd4e40367d4feeeee5674e967ea03a2e27d3efe
Krzysztof Lewandowski <krzlew1@gmail.com>
2023-07-13 23:46:32 +0200

    Updated power script
    Signed-off-by: Krzysztof Lewandowski <krzlew1@gmail.com>

--- scripts/scripts/power.sh
+++ scripts/scripts/power.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
-declare options=("suspend
-reboot
-shutdown")
+shutdown=" shutdown"
+suspend="󰒲 suspend"
+reboot="󰜉 reboot"
 
-choice=$(echo -e "${options[@]}" | \
-    rofi -dmenu -i -p "Power menu" \
+uptime=$(uptime -p | sed -e 's/up //g')
+
+options="$suspend\n$reboot\n$shutdown"
+
+choice=$(echo -e "$options" | \
+    rofi -dmenu -i -p "$uptime" \
     -theme-str 'window {width:20%; height: 20%;}')
 
 if [ $? -ne 0 ]; then
@@ -13,13 +17,13 @@ if [ $? -ne 0 ]; then
 fi
 
 case "$choice" in
-    suspend)
+    $suspend)
         systemctl suspend
     ;;
-    reboot)
+    $reboot)
         systemctl reboot
     ;;
-    shutdown)
+    $shutdown)
         systemctl poweroff
     ;;
 esac
