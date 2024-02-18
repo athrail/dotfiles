c65f3e03b7c86a0a7a422d610b7ccebfdfca3003
Krzysztof Lewandowski <krzlew1@gmail.com>
2024-02-18 14:33:24 +0100

    nvim: lazy update, config updates
    Updated harpoon keybind to CTRL + P for quicker access to common usecase
    Enable harpoon quick switch to CTRL + hjkl

--- .config/nvim/lua/config/harpoon.lua
+++ .config/nvim/lua/config/harpoon.lua
@@ -4,10 +4,10 @@ vim.keymap.set('n', '<leader>ja', function() harpoon:list():append() end, { desc
 vim.keymap.set('n', '<leader>jr', function() harpoon:list():remove() end, { desc = "[R]emove file from harpoon"})
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
--- vim.keymap.set("n", "<C-U>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-I>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-O>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-P>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-A-P>", function() harpoon:list():prev() end)
