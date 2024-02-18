7bb0f946f980d96343a5d62f575a56597bb85d8b
Krzysztof Lewandowski <krzlew1@gmail.com>
2024-02-19 00:17:27 +0100

    neovim: remap telescope and add split mappings

--- .config/nvim/lua/athrail/core/keymap.lua
+++ .config/nvim/lua/athrail/core/keymap.lua
@@ -1,22 +1,36 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+local keymap = vim.keymap -- for conciseness
+
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Buffer jumping
-vim.keymap.set('n', '<leader>n', function() vim.cmd('bn') end, { desc = 'Next buffer in buffer list' })
-vim.keymap.set('n', '<leader>p', function() vim.cmd('bN') end, { desc = 'Previous buffer in buffer list' })
-vim.keymap.set('n', '<leader>q', function() vim.cmd('bd') end, { desc = 'Delete current buffer' })
+keymap.set('n', '<leader>n', function() vim.cmd('bn') end, { desc = 'Next buffer in buffer list' })
+keymap.set('n', '<leader>p', function() vim.cmd('bN') end, { desc = 'Previous buffer in buffer list' })
+keymap.set('n', '<leader>q', function() vim.cmd('bd') end, { desc = 'Delete current buffer' })
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "[S]plit window [v]ertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "[S]plit window [h]orizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
