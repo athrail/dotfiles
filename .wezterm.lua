dc83e430c9a8e7c46b6c94f9e7fa4414d4dd732d
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-05-02 00:04:31 +0200

    wezterm: switched to wezterm and updated config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,69 +3,43 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.color_scheme = 'Tokyo Night'
-config.max_fps = 60
-config.use_fancy_tab_bar = false
-config.window_decorations = 'NONE'
+-- rose-pine theme for wezterm
+local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
+-- smart-splits for neovim integration
+local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
 
-config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 0,
-  bottom = 0,
-}
+config.colors = theme.colors()
+config.window_frame = theme.window_frame()
+config.max_fps = 60
+config.use_fancy_tab_bar = true
+config.window_decorations = 'NONE'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 14.0
 
 config.leader = { key = 'Space', mods = 'CTRL' }
 config.keys = {
-  -- new tab
-  {
-    key = 'c',
-    mods = 'LEADER',
-    action = act.SpawnTab 'CurrentPaneDomain',
-  },
-  {
-    key = 'h',
-    mods = 'LEADER',
-    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'v',
-    mods = 'LEADER',
-    action = act.SplitPane {
-      direction = 'Right',
-      size = { Percent = 50 },
-    },
-  },
-  {
-    key = 'L',
-    mods = 'ALT|SHIFT',
-    action = act.ActivateTabRelative(1),
-  },
-  {
-    key = 'H',
-    mods = 'ALT|SHIFT',
-    action = act.ActivateTabRelative(-1),
-  },
-  {
-    key = 'H',
-    mods = 'CTRL',
-    action = act.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'J',
-    mods = 'CTRL',
-    action = act.ActivatePaneDirection 'Down',
-  },
-  {
-    key = 'K',
-    mods = 'CTRL',
-    action = act.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'L',
-    mods = 'CTRL',
-    action = act.ActivatePaneDirection 'Right',
-  },
+  { key = 'c', mods = 'LEADER', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'h', mods = 'LEADER', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = 'v', mods = 'LEADER', action = act.SplitPane { direction = 'Right', size = { Percent = 50 } } },
+  { key = 'l', mods = 'ALT|SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'h', mods = 'ALT|SHIFT', action = act.ActivateTabRelative(-1) },
+  { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right' },
 }
+
+-- configure smart-splits plugin
+  -- smart_splits.apply_to_config(config)
+smart_splits.apply_to_config(config, {
+  direction_keys = { 'h', 'j', 'k', 'l' },
+  -- modifier keys to combine with direction_keys
+  modifiers = {
+    move = 'CTRL', -- modifier to use for pane movement, e.g. CTRL+h to move left
+    resize = 'META', -- modifier to use for pane resize, e.g. META+h to resize to the left
+  },
+  -- log level to use: info, warn, error
+  log_level = 'info',
+})
 
 return config
