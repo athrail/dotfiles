cf546a63d9ce47f3e2936b833ff4a60706de8504
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-05-07 08:45:02 +0200

    fish: restore fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+# general variables setup
+set fish_greeting
+
+# exports
+export MANPAGER='nvim +Man!'
+
+# aliases
+alias ff fastfetch
+alias .. "cd .."
+alias ls eza
+alias cl clear
+alias cat bat
+alias lg lazygit
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+fzf --fish | source
+zoxide init fish | source
+
+function update-mirrors
+    sudo reflector --latest 30 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+end
