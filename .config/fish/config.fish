d16ef47e89638b8f15742c32d6a306dc6104254a
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-07-29 20:21:15 +0200

    fish: bob and back to nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,15 @@
 # general variables setup
 set fish_greeting
 
-fish_add_path "/home/athrail/.local/bin"
+fish_add_path '/home/athrail/.local/bin'
 
 # exports
-set -xU EDITOR hx
+set -xU EDITOR 'nvim'
+set -xU MANPAGER 'nvim +Man!'
 
 # aliases
 alias ff fastfetch
-alias .. "cd .."
+alias .. 'cd ..'
 alias ls eza
 alias cl clear
 alias cat bat
@@ -17,7 +18,7 @@ alias hx helix
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fastfetch --colors-block-range-end 7 --colors-block-width 3
+    # fastfetch --colors-block-range-end 7 --colors-block-width 3
 end
 
 starship init fish | source
