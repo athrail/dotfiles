d35c7a7782e28e4773e9cc47c3917c2d9d769962
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-08-18 14:20:13 +0200

    fish: updates for rustup, bob

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set fish_greeting
 fish_add_path '/home/athrail/.local/bin'
 
 # exports
+set -xU VISUAL '/home/athrail/.local/share/bob/nvim-bin/nvim'
 set -xU EDITOR 'nvim'
 set -xU MANPAGER 'nvim +Man!'
 
@@ -24,6 +25,9 @@ end
 starship init fish | source
 fzf --fish | source
 zoxide init fish | source
+
+source "$HOME/.cargo/env.fish"
+source "$HOME/.local/share/bob/env/env.fish"
 
 function update-mirrors
     sudo reflector --latest 30 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
