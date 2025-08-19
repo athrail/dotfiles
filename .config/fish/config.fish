884e579f63d87e4844e0ee26547ab967cb19c03d
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-08-19 15:57:15 +0200

    fish: cleanup after coming back to arch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,18 @@
 # general variables setup
 set fish_greeting
 
-fish_add_path '/home/athrail/.local/bin'
+fish_add_path "/home/athrail/.local/bin"
 
 # exports
-set -xU VISUAL '/home/athrail/.local/share/bob/nvim-bin/nvim'
-set -xU EDITOR 'nvim'
-set -xU MANPAGER 'nvim +Man!'
+set -xU VISUAL "/home/athrail/.local/share/bob/nvim-bin/nvim"
+set -xU EDITOR "nvim"
+set -xU MANPAGER "nvim +Man!"
+
+source "$HOME/.local/share/bob/env/env.fish"
 
 # aliases
 alias ff fastfetch
-alias .. 'cd ..'
+alias .. "cd .."
 alias ls eza
 alias cl clear
 alias cat bat
@@ -25,9 +27,6 @@ end
 starship init fish | source
 fzf --fish | source
 zoxide init fish | source
-
-source "$HOME/.cargo/env.fish"
-source "$HOME/.local/share/bob/env/env.fish"
 
 function update-mirrors
     sudo reflector --latest 30 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
