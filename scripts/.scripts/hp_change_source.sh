f02011924c5acc51cb50c38b8338e1f8e4b2f543
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-12-12 08:14:23 +0100

    scripts: adding monitor source change script

---
+++ scripts/.scripts/hp_change_source.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+menu() {
+  local prompt="Select source"
+  local options="Type-C\nDisplay Port\nHDMI"
+
+  local selection=$(echo -e "$options" | fuzzel --lines 3 --dmenu -p "$prompt…" "${args[@]}")
+  case $selection in
+  *Display\ Port*) output="0x0f" ;;
+  *HDMI*) output="0x11" ;;
+  *Type-C*) output="0x13" ;;
+  esac
+
+  ddcutil setvcp 0x60 $output
+}
+
+menu
